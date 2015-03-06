9a6d2ba38ca5855344fb1f080e5f5bb756cf2335
Eren Emre <e3@e3s-Mac-Pro.local>
2015-03-06 14:12:50 -0800

    installed grunt sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,23 +1,17 @@
-module.exports = function(grunt) {
-
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+grunt.initConfig({
+  sass: {                              // Task
+    dist: {                            // Target
+      options: {                       // Target options
+        style: 'expanded'
       },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      files: {                         // Dictionary of files
+        'main.css': 'main.scss',       // 'destination': 'source'
+        'widgets.css': 'widgets.scss'
       }
     }
-  });
+  }
+});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-sass');
 
-  // Default task(s).
-  grunt.registerTask('default', ['uglify']);
-
-};
+grunt.registerTask('default', ['sass']);
